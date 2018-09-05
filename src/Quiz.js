97484ad7062c0350bf2fc8138dee292d70fcf521
Pasi Kuokkanen <pasi.kuokkanen@hiq.fi>
2018-09-05 15:13:35 +0300

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,8 +11,8 @@ class Quiz extends Component {
 
     render() {
         return <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position]} />
-            </div>
+            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position]} />
+        </div>
     }
 }
 
